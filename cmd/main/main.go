376318683f0057207f7d72f8e1be4a91751dcb11
Antony0101 <vimalt24@gmail.com>
2024-10-12 22:50:27 +0530

    started working ui lib fyne

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
 
-func main(){
-	fmt.Println("hello")
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
+)
+
+var mainApp fyne.App
+
+func main() {
+	mainApp = app.New()
+	mainWindow := mainApp.NewWindow("mainWindow")
+
+	textGrid := widget.NewTextGrid()
+	textGrid.SetText("hello world hello\n hehe hello")
+	mainWindow.SetContent(container.NewVBox(
+		widget.NewLabel("hello world"),
+		textGrid,
+	))
+	go closeWindow()
+	fmt.Println("running main window...")
+	mainWindow.ShowAndRun()
+
+}
+
+func closeWindow() {
+	time.Sleep(4 * time.Second)
+	fmt.Println("closing main window")
+	mainApp.Quit()
 }
